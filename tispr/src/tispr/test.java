dd75196d7c2b5c25883a627d369e5a78107a9902
olgakhlistun <olga.khlistun@gmail.com>
2016-10-26 22:47:14 -0700

    selenium upsate new version

--- tispr/src/tispr/test.java
+++ tispr/src/tispr/test.java
@@ -4,27 +4,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-//import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class test {
 
 	public static void main(String[] args) throws InterruptedException {
+		//Initialize webdriver
 		System.setProperty("webdriver.chrome.driver", "C:/Users/osik/Desktop/automatiom/chromedriver.exe");
-
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("chrome.switches", "--disable-extensions");
-
 		WebDriver myChrome = new ChromeDriver(options);
+		//Launch tispr
 		myChrome.get("https://tispr.com/");
 		myChrome.manage().window().maximize();
-
 		JavascriptExecutor jse = (JavascriptExecutor) myChrome;
+		//Scroll down
 		jse.executeScript("window.scrollBy(0,10250)", "");
 		Thread.sleep(1000);
+		//Scroll up
 		jse.executeScript("window.scrollBy(0,-10250)", "");
 		WebElement aboutUs = myChrome.findElement(By.id("menu-item-1264"));
+		//Click thru main menu buttons
 		aboutUs.click();
 		Thread.sleep(1000);
 		myChrome.navigate().back();
@@ -36,45 +37,35 @@ public class test {
 		blogLink.click();
 		Thread.sleep(1000);
 		myChrome.navigate().back();
-
+		//Get current handler
 		String winHandleBefore = myChrome.getWindowHandle();
 		WebElement bittenApple = myChrome.findElement(By.xpath("//*[@id='nav_download_link']/div/a[2]/img"));
 		// Perform the click operation that opens new window
 		bittenApple.click();
 		Thread.sleep(1000);
-		// myChrome.navigate().back();
-
 		// Switch to new window opened
 		for (String winHandle : myChrome.getWindowHandles()) {
 			myChrome.switchTo().window(winHandle);
 		}
 		Thread.sleep(2500);
-		// Close the new window, if that window no more required
+		// Close the new window
 		myChrome.close();
-
 		// Switch back to original browser (first window)
 		myChrome.switchTo().window(winHandleBefore);
-
 		// Continue with original browser (first window)
 		Thread.sleep(3000);
 		WebElement greenRobot = myChrome.findElement(By.xpath("//*[@id='nav_download_link']/div/a[1]/img"));
 		// Perform the click operation that opens new window
 		greenRobot.click();
 		Thread.sleep(1000);
-		// myChrome.navigate().back();
-
 		// Switch to new window opened
 		for (String winHandle : myChrome.getWindowHandles()) {
 			myChrome.switchTo().window(winHandle);
 		}
 		Thread.sleep(2500);
-		// Close the new window, if that window no more required
+		// Close the new window
 		myChrome.close();
-
 		// Switch back to original browser (first window)
 		myChrome.switchTo().window(winHandleBefore);
-
-		
-		// Continue with original browser (first window)
 	}
 }
